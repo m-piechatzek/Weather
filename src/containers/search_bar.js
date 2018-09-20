140f0b713df7569b3288f17e7880c2bc2ea08fdf
MonikaP <piechatzek@hotmail.com>
2018-09-19 18:24:01 -0600

    Added sumbit button for search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,6 +5,11 @@ export default class SearchBar extends React.Component {
 		return (
 			<form className="input-group">
 				<input />
+				<span className="input-group-btn">
+					<button type="submit" className="btn btn-secondary">
+						Submit
+					</button>
+				</span>
 			</form>
 		)
 	}
