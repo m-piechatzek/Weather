4c51b6d331678714797218808d0a5d5efba57d3f
MonikaP <piechatzek@hotmail.com>
2018-09-22 17:33:36 -0600

    Created google maps file and added more graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Chart from '../components/chart'
 
 class WeatherList extends React.Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name
-		const temp = cityData.list.map(weather => weather.main.temp)
+		const temp = _.map(
+			cityData.list.map(weather => weather.main.temp),
+			temps => temps - 273
+		)
+		const pressures = cityData.list.map(weather => weather.main.pressure)
+		const humidities = cityData.list.map(weather => weather.main.humidity)
 		console.log(temp)
 
 		return (
 			<tr key={name}>
 				<td>{name}</td>
+				<td>
+					<Chart color="blue" data={temp} units="C" />
+				</td>
+				<td>
+					<Chart color="red" data={pressures} units="hPa" />
+				</td>
+				<td>
+					<Chart color="orange" data={humidities} units="%" />
+				</td>
 			</tr>
 		)
 	}
@@ -19,9 +34,9 @@ class WeatherList extends React.Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (C)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 
